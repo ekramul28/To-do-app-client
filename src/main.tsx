c32d11cd53559fbec74eda6395debf5e08bf3105
Ekramul <mdekramulhassan168@gmail.com>
2025-02-26 08:58:41 +0600

    redux added and route dome

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
 import "./index.css";
 import { Provider } from "react-redux";
-import { store } from "./redux/store.ts";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistor, store } from "./redux/store.ts";
+import { Toaster } from "sonner";
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <RouterProvider router={router} />
+        <Toaster />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
